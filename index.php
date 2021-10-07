e742349bd1fd93f988e92f63dff6bb74d341dd0e
SimoneBenato <sb270298@gmail.com>
2021-10-07 17:10:09 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!-- Creare una variabile con un paragrafo di testo a vostra scelta. -->
+<?php
+    $message = 'Lorem ipsum, but dolor sit amet consectetur adipisicing elit. Similique vero ipsum exercitationem enim vel quo facilis but incidunt cum odit labore nemo, but quisquam but sapiente dignissimos nihil qui ipsam alias quam quos!';
+    //Una parola da censurare viene passata dall'utente tramite parametro GET.
+    $badWord = $_GET['word'];
+?>
+
+<!-- Stampare a schermo il paragrafo e la sua lunghezza. -->
+<p> <?php echo str_replace($badWord, '***', $message) ?> </p>
+<!-- Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
+
